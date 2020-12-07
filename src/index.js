39db95a122119d4845500edc5667306a50e7757d
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-07 05:01:00 -0500

    working on search bar

--- src/index.js
+++ src/index.js
@@ -17,6 +17,31 @@ window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("write-review-form").innerHTML="";
   })
 
+  document.getElementById("search-bar").addEventListener("input", e => {
+      
+      fetch(`${HOME_URL}businesses`)
+        .then(response => response.json())
+        .then(response => {
+          const resultDisplay = document.getElementById("search-result")
+            response.forEach(obj => {
+              if (obj.name.toLowerCase().indexOf(e.target.value) > -1){
+                let node = document.createElement("a")
+                node.setAttribute("href","#")
+                node.innerText = obj.name
+                node.addEventListener("click", e => {
+                  Business.loadBusiness(obj)
+                })
+                if (e.target.value.length < 5) resultDisplay.appendChild(node)
+                console.log("this is the search key", e.target.value)
+                console.log("this is the result ", obj.name)
+              }
+
+
+
+            })    
+        })
+  })
+
 })
   
 

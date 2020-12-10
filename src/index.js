7fae0cb142dba3a784858ad0a14da405d281f267
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-10 06:08:32 -0500

    fix search feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+
+
 window.addEventListener('DOMContentLoaded', (event) => {
 
+  // console.log(process.env.GOOGLE_KEY)
   if (localStorage.getItem("current_user")) {
     App.run();
   } else {
@@ -18,30 +21,32 @@ window.addEventListener('DOMContentLoaded', (event) => {
   })
 
   document.getElementById("search-bar").addEventListener("input", e => {
-    let result = []
-      fetch(`${HOME_URL}businesses`)
-        .then(response => response.json())
-        .then(response => {
-          const resultDisplay = document.getElementById("search-result");
-            response.forEach(obj => {
-              if (obj.name.toLowerCase().indexOf(e.target.value) > -1){
-                result.push(obj)
-                let node = document.createElement("a")
-                node.setAttribute("href","#")
-                node.setAttribute("class","list-group-item list-group-item-action")
-                node.innerText = obj.name
-                node.addEventListener("click", e => {
-                  document.getElementById("search-bar").reset();
-                  Business.loadBusiness(obj);
-                })
-                if (resultDisplay.childNodes.length < 5) resultDisplay.appendChild(node)
-              } 
-            })    
-        })
-    document.getElementById("search-bar-submit").addEventListener("click", e => {
-      document.getElementById("search-bar").reset();
-      displaySearchResult(result);
-    })
+    if (e.target.value.length > 3){
+      let result = []
+        fetch(`${HOME_URL}businesses`)
+          .then(response => response.json())
+          .then(response => {
+            const resultDisplay = document.getElementById("search-result");
+              response.forEach(obj => {
+                if (obj.name.toLowerCase().indexOf(e.target.value) > -1){
+                  result.push(obj)
+                  let node = document.createElement("a")
+                  node.setAttribute("href","#")
+                  node.setAttribute("class","list-group-item list-group-item-action")
+                  node.innerText = obj.name
+                  node.addEventListener("click", e => {
+                    document.getElementById("search-bar").reset();
+                    Business.loadBusiness(obj);
+                  })
+                  if (resultDisplay.childNodes.length < 5) resultDisplay.appendChild(node)
+                } 
+              })    
+          })
+      document.getElementById("search-bar").addEventListener("submit", e => {
+        document.getElementById("search-bar").reset();
+        displaySearchResult(result);
+      })
+    }
   })
 
 
